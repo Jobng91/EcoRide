9b2f0c800ee6512e2de56aaf04058c3ec90974d5
Jobng91 <jobng91@gmail.com>
2025-02-13 23:04:29 +0100

    Fin html css

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,19 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
+    new Route("/detail", "Detail", "/pages/detail.html"),
+    new Route("/profil", "Profil", "/pages/profil.html"),
+    new Route("/course", "Course", "/pages/course.html"),
+    new Route("/espaceAdmin", "Espace Admin", "/pages/espaceAdmin.html"),
+    new Route("/espaceChauffeur", "Espace Chauffeur", "/pages/espaceChauffeur.html"),
+    new Route("/espaceEmploye", "Espace Employe", "/pages/espaceEmploye.html"),
+    new Route("/espacePassager", "Espace Passager", "/pages/espacePassager.html"),
+    new Route("/validation", "Validation", "/pages/validation.html"),
+    new Route("/inscription", "Inscription", "/pages/inscription.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+export const websiteName = "EcoRide";
