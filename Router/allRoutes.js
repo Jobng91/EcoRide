020f7f9968d1fac3a8f8575b974ec594b21856b4
Jobng91 <jobng91@gmail.com>
2025-02-15 09:27:57 +0100

    Hmtl css final

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,18 +3,18 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
-    new Route("/detail", "Detail", "/pages/detail.html"),
+    new Route("/covoiturage", "Covoiturage", "/pages/covoiturage/covoiturage.html"),
+    new Route("/detail", "Detail", "/pages/covoiturage/detail.html"),
     new Route("/profil", "Profil", "/pages/profil.html"),
-    new Route("/course", "Course", "/pages/course.html"),
-    new Route("/espaceAdmin", "Espace Admin", "/pages/espaceAdmin.html"),
-    new Route("/espaceChauffeur", "Espace Chauffeur", "/pages/espaceChauffeur.html"),
-    new Route("/espaceEmploye", "Espace Employe", "/pages/espaceEmploye.html"),
-    new Route("/espacePassager", "Espace Passager", "/pages/espacePassager.html"),
-    new Route("/validation", "Validation", "/pages/validation.html"),
-    new Route("/inscription", "Inscription", "/pages/inscription.html"),
-    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/course", "Course", "/pages/covoiturage/course.html"),
+    new Route("/espaceAdmin", "Espace Admin", "/pages/espace/espaceAdmin.html"),
+    new Route("/espaceChauffeur", "Espace Chauffeur", "/pages/espace/espaceChauffeur.html"),
+    new Route("/espaceEmploye", "Espace Employe", "/pages/espace/espaceEmploye.html"),
+    new Route("/espacePassager", "Espace Passager", "/pages/espace/espacePassager.html"),
+    new Route("/validation", "Validation", "/pages/covoiturage/validation.html"),
+    new Route("/inscription", "Inscription", "/pages/auth/inscription.html", "js/auth/signup.js"),
+    new Route("/connexion", "Connexion", "/pages/auth/connexion.html"),
 ];
-
+    
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
---
+++ js/auth/signup.js
@@ -0,0 +1,45 @@
+//Implémenter le JS de ma page
+
+const inputPseudo = document.getElementById("PseudoInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+
+inputPseudo.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidationPassword.addEventListener("keyup", validateForm);
+
+//Function permettant de valider tout le formulaire
+function validateForm(){
+    validateRequired(inputMail);
+    validateRequired(inputPseudo);
+}
+
+function validateRequired(input){
+    if(input.value != ''){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+    }
+}
+
+
+function validateMail(input){
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;   
+    if(mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
